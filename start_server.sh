ab711f1725e6b8b9c52fcb1bdbe660552b45148e
root <danlowe95@gmail.com>
2018-01-31 02:34:58 +0000

    wip

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ./Steam/csgo-ds
-screen -S "Happy Little Accidents Server" ./srcds_run -game csgo -usercon -port 27015 -tickrate 128 +game_type 0 +game_mode 1 +mapgroup mg_bomb +map de_inferno -maxplayers_override 16 +sv_setsteamaccount BE03102B22DCBAB94EE0C42806007C33 -net_port_try 1
+cd /root/Steam/csgo-ds
+screen -dS "Happy Little Accidents Server" ./srcds_run -game csgo -usercon -port 27015 -tickrate 128 +game_type 0 +ip 0.0.0.0 +net_public_adr 104.236.80.253 +game_mode 1 +mapgroup mg_bomb +map de_inferno -maxplayers_override 16 +sv_setsteamaccount BE03102B22DCBAB94EE0C42806007C33 -net_port_try 1
 
